e96a02125fa7437af35af0e72c3010a8741aa102
Stav Green <stavgn@gmail.com>
2022-01-19 21:15:51 +0200

    wip

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -5,20 +5,53 @@ extern "C"
 #include <stdio.h>
 }
 
-MallocMetaData *LINK_START = NULL;
-MallocMetaData *LINK_END = NULL;
+MallocMetaData *LINK_FREE_START = NULL;
+MallocMetaData *LINK_FREE_END = NULL;
+
+MallocMetaData *LINK_USED_START = NULL;
+MallocMetaData *LINK_USED_END = NULL;
 
 class MallocMetaData
 {
     bool free = false;
     size_t size;
     MallocMetaData *next;
-    MallocMetaData &prev;
+    MallocMetaData *prev;
+    void *data_ptr = nullptr;
     MallocMetaData(size_t size) : size(size);
 };
 
+MallocMetaData *find_slot(size_t size)
+{
+    for (MallocMetaData iter = LINK_START; i != LINK_END; iter = iter->next)
+    {
+        if (iter->size <= size)
+        {
+            return iter;
+        }
+    }
+    return nullptr;
+}
+
 void *smalloc(size_t size)
 {
+    if (size == 0)
+    {
+        return NULL;
+    }
+
+    MallocMetaData slot * = find_slot(size);
+    if (slot == nullptr)
+    {
+        ptr = sbrk(_size_meta_data() + size);
+        if (ptr == (void *)-1)
+        {
+            return NULL;
+        }
+        MallocMetaData data(size);
+        sprintf(ptr, "%s", ) return ptr;
+    }
+    insert_data_to_link()
 }
 
 void *scalloc(size_t num, size_t size)
